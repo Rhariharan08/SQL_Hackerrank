cfda7e647bebf29d08551aba0fd0a1330b6b9dcd
Rhariharan08 <hariharan081103@gmail.com>
2024-06-04 08:56:06 +0530

    Add files via upload

---
+++ 1.Revising_the_Select_Query_I.sql
@@ -0,0 +1,11 @@
+/*
+  Problem Link -> https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+*/
+
+select *
+from city
+where population > 100000
+and countrycode = 'usa'
+
+
+
